24b5ef4f98715660edf28b559287e6b92f112165
Bulat Zalyaliev <bzalyaliev@gmail.com>
2023-02-16 15:04:57 +0300

    Initial commit

---
+++ src/main/java/com/github/bzalyaliev/catsapi/CatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.bzalyaliev.catsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsApiApplication.class, args);
+    }
+
+}
